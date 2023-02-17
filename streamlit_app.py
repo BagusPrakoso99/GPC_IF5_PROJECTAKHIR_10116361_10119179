f35ffcb7c36fc3665d3bddaa146a840965dd32be
BagusPrakoso99 <125698332+BagusPrakoso99@users.noreply.github.com>
2023-02-18 03:27:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.subheader("Upload Gambar dan Edit secara Real-Time!!!")
 #Sidebar
 st.sidebar.markdown('<p class="font">Aplikasi Edit Gambar</p>', unsafe_allow_html=True)
 with st.sidebar.expander("Tentang Aplikasi"):
-     st.write("""Aplikasi Simpel Mudah dan Cepat digunakan untuk mengedit gambar dalam format JPG,PNG dan JPEG yang memanfaatkan library openCV, Streamlit dan Numpy.
+     st.write("""Aplikasi Simpel Mudah dan Cepat digunakan untuk mengedit gambar dalam format JPG,PNG dan JPEG yang memanfaatkan library openCV, Streamlit dan Numpy.\n10116361 Reka Saepul Anwar
      """)
 
 #Upload Gambar
